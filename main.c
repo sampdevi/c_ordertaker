795c5b3ec97a2c1b64f36f5e449f88eca512da00
Sean Denzel Y. Robenta (zel) <38722112+sampdevi@users.noreply.github.com>
2023-09-23 13:51:51 +0800

    Finalized main.c

--- main.c
+++ main.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <conio.h>
 
 #define RECEIPT_HEADER_LINE_1  "MyKitchen\n" // 24
 #define RECEIPT_HEADER_LINE_2  "Taft Avenue, Manila\n" // 29
 #define RECEIPT_HEADER_LINE_3  "Your Receipt\n" // 26
+
+#define RECEIPT_FOOTER_LINE_1  "Thank you very much!\n" // 24
+#define RECEIPT_FOOTER_LINE_2  "See you again!" // 29
 #define RECEIPT_LENGTH  40
 
 #define MAX_PRODUCTS    3
@@ -28,7 +32,7 @@ float productPrices[MAX_PRODUCTS] = {
 
 int productCart[MAX_PRODUCTS];
 
-void printReceipt(char date);
+void printReceipt(int date);
 void printLine();
 
 void resetProductCart();
@@ -36,8 +40,11 @@ void listProducts();
 void initiateOrderTaker();
 void initiateOrderTakerDateInput();
 int  addToCart();
+void displayCart();
 
 void dateParser(int date);
+
+float getTax(float gross);
 
 int isValidProduct(int _productId);
 
@@ -48,6 +55,7 @@ void main() {
         "Waiter! pa-order naman ako ng (?)\n"
     );
 
+    system("cls");
     listProducts();
     initiateOrderTaker();
 }
@@ -65,36 +73,60 @@ void listProducts()
 void initiateOrderTaker()
 {
     int productId = -1;
-    printf("%sSelect Product ID (0 to complete order):\n> ");
-    scanf("%d", &productId);
+    printf("Select Product ID (0 to complete order):\n> ");
+
+    if(scanf("%d", &productId) != 1) {
+        // this ^ receives an invalid input (if what is inputted is not an integer), then proceeds to this error catching code
+        int c;
+
+        // - medyo complex, pero this while loop catches any output from the terminal,
+        // - until it returns EOF (end of file), until then, it will not output anything,
+        // - avoiding the previous spamming initiateOrderTaker again and again.
+        while ((c = getchar()) != EOF && c != '\n') continue;
+
+        printf("[ERROR]: Invalid input. Please try again.\n");
+        productId = -1;
+        return initiateOrderTaker();
+    }
 
     if(!isValidProduct(productId))
     {
-        printf("[ERROR]: Invalid product Id. Please try again.");
+        printf("[ERROR]: Invalid product Id. Please try again.\n");
         return initiateOrderTaker();
     }
     if(productId == 0)
     {
-        return initiateOrderTakeDateInput();
+        return initiateOrderTakerDateInput();
     }
     addToCart(productId - 1);
+    displayCart();
+    return initiateOrderTaker();
 }
 
 void initiateOrderTakerDateInput()
 {
-    printf("%s%s%s"
-        "[OrderTaker] > Main Menu > Products > Confirm > Date",
+    printf("%s%s%s",
+        "[OrderTaker] > Main Menu > Products > Confirm > Date\n",
         "Input the date today:",
         "\n> "
     );
-    char inputDate[64];
-    scanf("%s", inputDate);
+    int inputDate;
+    if(scanf("%d", &inputDate) != 1) {
+        int c;
+
+        while ((c = getchar()) != EOF && c != '\n') continue;
+
+        printf("[ERROR]: Invalid input. Please try again.\n");
+        inputDate = -1;
+        return initiateOrderTakerDateInput();
+    }
 
     printReceipt(inputDate);
 }
 
-void printReceipt(char date)
+void printReceipt(int date)
 {
+    system("cls");
     printLine();
     printf(
         "%24s%29s%26s\n\n", 
@@ -102,7 +134,7 @@ void printReceipt(char date)
         RECEIPT_HEADER_LINE_2,
         RECEIPT_HEADER_LINE_3
     );
-    print("%s", date);
+    dateParser(date); printf("\n");
     printLine();
 
     int totalItems = 0;
@@ -117,14 +149,28 @@ void printReceipt(char date)
             grandPrice += productCart[i] * productPrices[i];
             printf("%s\n", productNames[i]);
             printf("%10d x %.2f", productCart[i], productPrices[i]);
-            printf("%30.2f", productCart[i] * productPrices[i]);
+            printf("%22.2f", productCart[i] * productPrices[i]);
             printf("\n\n");
         }
     }
     printLine();
-    printf("Total %d item(s)", totalItems);
-    printf("%40d\n", grandPrice);
+    printf("Total %2d item(s)", totalItems);
+    printf("%24.2f\n", totalPrice);
+
+    printf("Sales Tax");
+    printf("%31.2f\n", getTax(totalPrice));
+    printLine();
+
+    printf("Grand Total");
+    printf("%29.2f\n\n", grandPrice);
     
+    printf("Tip Guide:");
+    printf("%18s15%% = %5.2f\n%28s18%% = %5.2f\n%28s20%% = %5.2f\n\n", "", grandPrice * TIP1, "", grandPrice * TIP2, "", grandPrice * TIP3);
+
+    printf("%5s%13s",
+        RECEIPT_FOOTER_LINE_1,
+        RECEIPT_FOOTER_LINE_2
+    );
     resetProductCart();
 }
 
@@ -135,7 +181,7 @@ void dateParser(int date)
     dd = date / 10000 % 100;
     mm = date / 1000000;
 
-    printf("%s/%s/%s", mm, dd, yyy);
+    printf("%d/%d/%d", mm, dd, yyy);
 }
 
 void printLine()
@@ -162,35 +208,27 @@ int addToCart(int productId)
     return 1;
 }
 
+void displayCart()
+{
+    system("cls");
+    listProducts();
+    printLine();
+    printf("\n[Current Cart Items]\n");
+    for(int i = 0; i < MAX_PRODUCTS; i++)
+    {
+        if(productCart[i] == 0) continue;
+        else {
+            printf("%s x%d\n", productNames[i], productCart[i]);
+        }
+    }
+}
 
 int isValidProduct(int _productId)
 {
-    return (_productId > MAX_PRODUCTS || _productId < 0);
-}
-
-// date
-// total_price = gross x 1.12
-// tax = gross x 0.12
-// gross = total_price / (1 + TAX)
-
-float computeTotal (int nOrders)
-{
-    float fTotal;
-    fTotal = nOrders * AMOUNT;
-    return fTotal;
-}
-
-float getTotalPrice(float gross)
-{
-    return gross * 1.12;
+    return (_productId > MAX_PRODUCTS || _productId < 0) ? 0 : 1;
 }
 
 float getTax(float gross)
 {
     return gross * 0.12;
-}
-
-float getGross(float total_price)
-{
-    return total_price / (1 + TAX);
 }
